3fbc5299fcf41fa0d8529974e10b01e93cd46498
Elliot Maisl <elliot.maisl@gmail.com>
2021-05-13 15:25:01 +0200

    fix(ConfigurationManager): query the right channel

--- src/lib/structures/ConfigurationManager.ts
+++ src/lib/structures/ConfigurationManager.ts
@@ -2,8 +2,7 @@ import { Collection } from 'discord.js';
 import Configuration from '@/models/configuration';
 import type MonkaClient from '@/structures/MonkaClient';
 import type { GuildTextBasedChannel } from '@/types';
-import { ConfigEntries } from '@/types/database';
-import type { ConfigurationDocument } from '@/types/database';
+import type { ConfigEntries, ConfigurationDocument } from '@/types/database';
 import { nullop } from '@/utils';
 
 export default class ConfigurationManager {
@@ -14,22 +13,22 @@ export default class ConfigurationManager {
 
   public async set(channel: ConfigEntries, value: GuildTextBasedChannel): Promise<void> {
     await Configuration.findOneAndUpdate(
-      { guild: value.guild.id, name: ConfigEntries.ModeratorFeedback },
+      { guild: value.guild.id, name: channel },
       { guild: value.guild.id, value: value.id },
       { upsert: true },
     );
-    this.channels.set(value.guild.id, { [channel]: value });
+    this.channels.set(value.guild.id, { [channel]: value } as Record<ConfigEntries, GuildTextBasedChannel>);
   }
 
   public async get(guildID: string, channel: ConfigEntries): Promise<GuildTextBasedChannel> {
     if (this.channels.get(guildID)?.[channel])
       return this.channels.get(guildID)[channel];
 
-    const result = await Configuration.findOne({ guild: guildID, name: ConfigEntries.ModeratorFeedback }).catch(nullop);
+    const result = await Configuration.findOne({ guild: guildID, name: channel }).catch(nullop);
     if (result?.value) {
       const resolvedChannel = this.client.channels.resolve(result.value);
       if (resolvedChannel.isText() && resolvedChannel.type !== 'dm') {
-        this.channels.set(guildID, { [channel]: resolvedChannel });
+        this.channels.set(guildID, { [channel]: resolvedChannel } as Record<ConfigEntries, GuildTextBasedChannel>);
         return resolvedChannel;
       }
     }
@@ -44,7 +43,9 @@ export default class ConfigurationManager {
     for (const channel of configuredChannels) {
       this.channels.set(
         channel.guild,
-        { [channel.name]: this.client.channels.resolve(channel.value) as GuildTextBasedChannel },
+        {
+          [channel.name]: this.client.channels.resolve(channel.value),
+        } as Record<ConfigEntries, GuildTextBasedChannel>,
       );
     }
   }
