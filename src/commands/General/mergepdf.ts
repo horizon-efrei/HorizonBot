62acc0b247716b06b228ab211c24c00c182c67b6
Wuhan-Imouto <61096082+Kethash@users.noreply.github.com>
2021-07-25 22:59:10 +0200

    lint:fix

--- src/commands/General/mergepdf.ts
+++ src/commands/General/mergepdf.ts
@@ -1,23 +1,25 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import type { Args, CommandOptions, Result, UserError } from '@sapphire/framework';
+import type {
+ Args, CommandOptions, Result, UserError,
+} from '@sapphire/framework';
+import type { Message } from 'discord.js';
 import PdfMerger from 'pdf-merger-js';
 import { mergePDF as config } from '@/config/commands/general';
 import MonkaCommand from '@/structures/MonkaCommand';
 import type { GuildMessage } from '@/types';
-import { Message } from 'discord.js';
 
 @ApplyOptions<CommandOptions>(config.options)
 export default class PdfMergeCommand extends MonkaCommand {
   public async run(message: GuildMessage, args: Args): Promise<void> {
     // Argument dans l'ordre croissant
-    const result : Result<Message[], UserError> = await args.repeatResult('message');
+    const result: Result<Message[], UserError> = await args.repeatResult('message');
 
-    let msgs : Message[] = [];
+    let msgs: Message[] = [];
 
-    if (result.success) 
+    if (result.success)
         msgs = result.value;
     else
-        message.channel.send("Erreur fichiers !")
+        await message.channel.send('Erreur fichiers !');
 
     const merger = new PdfMerger();
 
