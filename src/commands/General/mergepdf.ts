73e62bf5cbd9e5f6a142b5f080c8fb7a6dfc07c7
Wuhan-Imouto <61096082+Kethash@users.noreply.github.com>
2021-07-25 22:57:44 +0200

    Refactoring using repeatResult

--- src/commands/General/mergepdf.ts
+++ src/commands/General/mergepdf.ts
@@ -1,34 +1,29 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import type { Args, CommandOptions, UserError } from '@sapphire/framework';
+import type { Args, CommandOptions, Result, UserError } from '@sapphire/framework';
 import PdfMerger from 'pdf-merger-js';
-import pupa from 'pupa';
 import { mergePDF as config } from '@/config/commands/general';
 import MonkaCommand from '@/structures/MonkaCommand';
 import type { GuildMessage } from '@/types';
+import { Message } from 'discord.js';
 
 @ApplyOptions<CommandOptions>(config.options)
 export default class PdfMergeCommand extends MonkaCommand {
   public async run(message: GuildMessage, args: Args): Promise<void> {
     // Argument dans l'ordre croissant
+    const result : Result<Message[], UserError> = await args.repeatResult('message');
 
-    const msgIds: string[] = [];
-    while (!args.finished) {
-        const msgId = await args.pickResult('string');
-        if (msgId.success) {
-            msgIds.push(msgId.value);
-        } else {
-            const { parameter } = (config.messages.noPdfFound as UserError & { parameter: string });
-            await message.channel.send(pupa('{pdf_Error}', { pdfError: parameter }));
-            return;
-        }
-    }
+    let msgs : Message[] = [];
+
+    if (result.success) 
+        msgs = result.value;
+    else
+        message.channel.send("Erreur fichiers !")
 
     const merger = new PdfMerger();
 
-    for (const o of msgIds) {
-        const msg = message.channel.messages.cache.get(o);
-        for (const a of msg.attachments.values()) {
-            if (a.name.includes('.pdf'))
+    for (const o of msgs) {
+        for (const a of o.attachments.values()) {
+            if (a.name.endsWith('.pdf'))
                 merger.add(a.proxyURL);
         }
     }
