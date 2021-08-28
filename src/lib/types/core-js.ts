a729ec11c6f6444f93a32cb45e940642f3ba0c60
Elliot Maisl <elliot.maisl@gmail.com>
2021-08-28 17:14:48 +0200

    chore(typings): add more core-js typings

--- src/lib/types/core-js.ts
+++ src/lib/types/core-js.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-invalid-void-type, @typescript-eslint/no-explicit-any */
 
 export {};
 
@@ -15,9 +15,18 @@ declare global {
   // Define the new core-js collection-methods polyfill
   interface Set<T> {
     addAll(...values: T[]): this;
-    filter(predicate: (value: T, value2: T, set: Set<T>) => unknown, thisArg?: any): Set<T>;
-    filter<S extends T>(predicate: (value: T, value2: T, set: Set<T>) => value is S, thisArg?: any): Set<S>;
-    map<U>(callbackfn: (value: T, value2: T, set: Set<T>) => U, thisArg?: any): Set<U>;
+    deleteAll(...values: T[]): this;
+    every(predicate: (value: T, _: T, set: Set<T>) => unknown, thisArg?: any): boolean;
+    every<S extends T>(predicate: (value: T, _: T, set: Set<T>) => value is S, thisArg?: any): this is Set<S>;
+    filter(predicate: (value: T, _: T, set: Set<T>) => unknown, thisArg?: any): Set<T>;
+    filter<S extends T>(predicate: (value: T, _: T, set: Set<T>) => value is S, thisArg?: any): Set<S>;
+    find(predicate: (value: T, _: T, set: Set<T>) => unknown, thisArg?: any): T | undefined;
+    find<S extends T>(predicate: (this: void, value: T, _: T, set: Set<T>) => value is S, thisArg?: any): S | undefined;
     join(separator?: string): string;
+    map<U>(callbackfn: (value: T, _: T, set: Set<T>) => U, thisArg?: any): Set<U>;
+    reduce(callbackfn: (previousValue: T, currentValue: T, _: T, set: Set<T>) => T, initialValue: T): T;
+    reduce(callbackfn: (previousValue: T, currentValue: T, _: T, set: Set<T>) => T): T;
+    reduce<U>(callbackfn: (previousValue: U, currentValue: T, _: T, set: Set<T>) => U, initialValue: U): U;
+    some(predicate: (value: T, _: T, set: Set<T>) => unknown, thisArg?: any): boolean;
   }
 }
