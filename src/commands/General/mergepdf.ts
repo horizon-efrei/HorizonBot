6f3c3a47915962f5634e1515216f91137686d2d5
Wuhan-Imouto <61096082+Kethash@users.noreply.github.com>
2021-09-15 01:06:19 +0200

    Used filestream 'fs' to download pdf files

--- src/commands/General/mergepdf.ts
+++ src/commands/General/mergepdf.ts
@@ -1,11 +1,11 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import type {
- Args, CommandOptions,
-} from '@sapphire/framework';
+import type { Args, CommandOptions } from '@sapphire/framework';
 import PdfMerger from 'pdf-merger-js';
 import { mergePDF as config } from '@/config/commands/general';
 import HorizonCommand from '@/structures/commands/HorizonCommand';
 import type { GuildMessage } from '@/types';
+import envPaths from 'env-paths';
+import fs from 'fs';
 
 @ApplyOptions<CommandOptions>(config.options)
 export default class PdfMergeCommand extends HorizonCommand {
@@ -19,12 +19,20 @@ export default class PdfMergeCommand extends HorizonCommand {
     }
     const merger = new PdfMerger();
 
+    const tmpFolder = envPaths('horizonbot').temp;
+
     for (const msg of result.value) {
-        for (const fichier of msg.attachments.values()) {
-            if (fichier.name.endsWith('.pdf'))
-                merger.add(fichier.proxyURL);
+        for (const file of msg.attachments.values()) {
+            if (file.name.endsWith('.pdf'))
+                // Writing the pdf in tmp folder
+                fs.writeFileSync(tmpFolder,file.url);
+                // Add the file into the PdfMerger
+                merger.add(`${tmpFolder}/${file.name}`);
+                // Deleting the file
+                fs.unlinkSync(`${tmpFolder}/${file.name}`);
         }
     }
+    
 
     const pdfBuffer = await merger.saveAsBuffer();
 
