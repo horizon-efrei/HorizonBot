64f635448fa5955da8fb6df0cf6e087a5a476b8e
Elliot Maisl <elliot.maisl@gmail.com>
2021-10-06 15:54:51 +0200

    fix(emojiResolver): early return if no parameter

--- src/lib/resolvers/emoji.ts
+++ src/lib/resolvers/emoji.ts
@@ -6,6 +6,9 @@ import type { Guild } from 'discord.js';
 import nodeEmoji from 'node-emoji';
 
 export default function resolveEmoji(parameter: string, guild: Guild): Result<string, 'emojiError'> {
+  if (!parameter)
+    return err('emojiError');
+
   const regex = /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}\u{1F1E6}-\u{1F1FF}]/gu;
   const emoji = nodeEmoji.find(parameter)?.emoji
     || guild.emojis.cache.get(EmojiRegex.exec(parameter)?.[3])?.toString() || parameter.match(regex)?.[0];
