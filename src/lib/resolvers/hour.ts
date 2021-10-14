3c7f847d8ff7eadbb32a3a1ffb44f4c0ea8d2ff1
Elliot Maisl <elliot.maisl@gmail.com>
2021-10-14 20:08:13 +0200

    fix(resolveHour): allow one space

--- src/lib/resolvers/hour.ts
+++ src/lib/resolvers/hour.ts
@@ -2,7 +2,7 @@ import type { Result } from '@sapphire/framework';
 import { err, ok } from '@sapphire/framework';
 import type { HourMinutes } from '@/types';
 
-const HOUR_REGEX = /^(?<hour>\d{1,2})[h:]?(?<minutes>\d{2})?$/imu;
+const HOUR_REGEX = /^(?<hour>\d{1,2})[h:\s](?<minutes>\d{2})?$/imu;
 
 export default function resolveHour(parameter: string): Result<HourMinutes, 'hourError'> {
   const hour = Number.parseInt(HOUR_REGEX.exec(parameter)?.groups?.hour, 10);
