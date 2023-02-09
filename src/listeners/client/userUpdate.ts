577458c9009e4ff9e5157ad55645aca6d717259f
Elliot Maisl <elliot.maisl@gmail.com>
2023-02-09 22:41:18 +0100

    fix(logs): send ChangeUsername only in guild with member (fixes #93)

--- src/listeners/client/userUpdate.ts
+++ src/listeners/client/userUpdate.ts
@@ -2,18 +2,22 @@ import { Listener } from '@sapphire/framework';
 import type { User } from 'discord.js';
 import * as DiscordLogManager from '@/structures/DiscordLogManager';
 import { DiscordLogType } from '@/types/database';
+import { nullop } from '@/utils';
 
 export default class UserUpdateListener extends Listener {
   public async run(oldUser: User, newUser: User): Promise<void> {
     if (oldUser.username !== newUser.username) {
-      for (const guildId of this.container.client.guilds.cache.keys()) {
-        await DiscordLogManager.logAction({
-          type: DiscordLogType.ChangeUsername,
-          context: newUser.id,
-          content: { before: oldUser.username, after: newUser.username },
-          guildId,
-          severity: 1,
-        });
+      for (const [guildId, guild] of this.container.client.guilds.cache) {
+        const member = await guild.members.fetch({ user: newUser.id, force: false }).catch(nullop);
+        if (member) {
+          await DiscordLogManager.logAction({
+            type: DiscordLogType.ChangeUsername,
+            context: newUser.id,
+            content: { before: oldUser.username, after: newUser.username },
+            guildId,
+            severity: 1,
+          });
+        }
       }
     }
   }
