1d6b6f1b6a03ff64b4f32a3aaf8ed7208ce991db
Elliot Maisl <elliot.maisl@gmail.com>
2023-03-19 12:52:02 +0100

    chore(lint): add eslint-plugin-deprecation

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,17 @@ module.exports = {
   root: true,
   extends: ['noftalint/typescript'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'deprecation'],
   ignorePatterns: ['node_modules/', 'dist/'],
   reportUnusedDisableDirectives: true,
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
   rules: {
+    'deprecation/deprecation': 'warn',
+
     // It cannot resolve TypeScript's path aliases. See https://github.com/mysticatea/eslint-plugin-node/issues/233
     'node/no-missing-import': 'off',
-
-    // @typescript-eslint can't find the `.toString()` method for these types, but it
-    // does exists as it is inherited from the `Channel` class.
-    '@typescript-eslint/no-base-to-string': ['error', {
-      ignoredTypeNames: ['TextChannel', 'NewsChannel'],
-    }],
 
     // We don't necessarily want to use `this` in our class methods (such as `Command#run`),
     // but neither do we want them to be static.
