aebf4acdece777fe5b0459532a51208020b1dc2b
Elliot Maisl <elliot.maisl@gmail.com>
2023-04-02 12:18:22 +0200

    feat(date): add bespoke date format parsing

--- src/lib/resolvers/date.ts
+++ src/lib/resolvers/date.ts
@@ -1,12 +1,17 @@
 import type { Result } from '@sapphire/framework';
 import { err, ok } from '@sapphire/framework';
-import { fr /* French */ as chrono } from 'chrono-node';
+import { fr as chrono } from 'chrono-node';
 
 const isPast = (date: Date): boolean => date.getTime() < Date.now();
 
 export default function resolveDate(parameter: string, options?: { canBePast: boolean }): Result<Date, 'dateError'> {
-  const date = chrono.parseDate(parameter);
+  const expandedParameter = parameter
+    .replace(/ajrd/i, 'aujourd\'hui')
+    .replace(/apr[eè]s[ -]demain/i, 'dans 2 jours');
 
+  const date = chrono.parseDate(expandedParameter);
+
+  // TODO: Separate error for past dates
   if (!date || (!options?.canBePast && isPast(date)))
     return err('dateError');
   return ok(date);
