6f5445ff266a4dabd84328df162e6f7147ff6662
Elliot Maisl <elliot.maisl@gmail.com>
2023-04-11 11:01:44 +0200

    fix(date): parse weekdays better

--- src/lib/resolvers/date.ts
+++ src/lib/resolvers/date.ts
@@ -2,12 +2,34 @@ import type { Result } from '@sapphire/framework';
 import { err, ok } from '@sapphire/framework';
 import { fr as chrono } from 'chrono-node';
 
+const days = {
+  lundi: 1,
+  mardi: 2,
+  mercredi: 3,
+  jeudi: 4,
+  vendredi: 5,
+  samedi: 6,
+  dimanche: 0,
+};
+
+const nextDay = (day: keyof typeof days): number => ((days[day] - new Date().getDay()) + 7) % 8;
+
 const isPast = (date: Date): boolean => date.getTime() < Date.now();
 
 export default function resolveDate(parameter: string, options?: { canBePast: boolean }): Result<Date, 'dateError'> {
   const expandedParameter = parameter
     .replace(/ajrd/i, 'aujourd\'hui')
-    .replace(/apr[eè]s[ -]demain/i, 'dans 2 jours');
+    .replace(/apr[eè]s[ -]demain/i, 'dans 2 jours')
+    .replace(/dm/i, 'demain')
+    .replace(/apdm/i, 'dans 2 jours')
+    // With the French locale, chrono-node fails to parse weekdays correctly. If you say "monday" but you are tuesday,
+    // it will reference the day before, not the next monday, and if you say "next monday" it will reference the monday
+    // in two weeks. You have effectively no way to tell it "this monday" with the French locale.
+    // Thus the following hack.
+    .replace(
+      new RegExp(`(${Object.keys(days).join('|')})`, 'i'),
+      (_, day: keyof typeof days) => `dans ${nextDay(day)} jours`,
+    );
 
   const date = chrono.parseDate(expandedParameter);
 
